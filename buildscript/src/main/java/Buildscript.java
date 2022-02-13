c3d084cc1adde5b6b37a43149f094c8a4c9c95e1
DragonEggBedrockBreaking <cuber3million@gmail.com>
2022-02-13 12:08:37 +0000

    Initial mod commit (2.0)

---
+++ buildscript/src/main/java/Buildscript.java
@@ -0,0 +1,81 @@
+import java.nio.file.Path;
+import io.github.coolcrabs.brachyura.decompiler.BrachyuraDecompiler;
+import io.github.coolcrabs.brachyura.decompiler.fernflower.FernflowerDecompiler;
+import io.github.coolcrabs.brachyura.fabric.FabricLoader;
+import io.github.coolcrabs.brachyura.fabric.FabricMaven;
+import io.github.coolcrabs.brachyura.fabric.FabricProject;
+import io.github.coolcrabs.brachyura.fabric.Yarn;
+import io.github.coolcrabs.brachyura.maven.Maven;
+import io.github.coolcrabs.brachyura.maven.MavenId;
+import io.github.coolcrabs.brachyura.processing.ProcessorChain;
+import net.fabricmc.mappingio.tree.MappingTree;
+
+public class Buildscript extends FabricProject {
+    @Override
+    public String getMcVersion() {
+        // Minecraft Version
+        return "1.18.1";
+    }
+
+    @Override
+    public MappingTree createMappings() {
+        // Yarn Mappings Version
+        return Yarn.ofMaven(FabricMaven.URL, FabricMaven.yarn("1.18.1+build.22")).tree;
+    }
+
+    @Override
+    public FabricLoader getLoader() {
+        // Fabric Loader Version
+        return new FabricLoader(FabricMaven.URL, FabricMaven.loader("0.13.1"));
+    }
+
+    @Override
+    public String getModId() {
+        // Mod Name
+        return "ultra_pickaxe";
+    }
+
+    @Override
+    public String getVersion() {
+        // Mod Version
+        return "0.0.1";
+    }
+
+    @Override
+    public void getModDependencies(ModDependencyCollector d) {
+        // Fabric API
+        d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api", "fabric-resource-loader-v0", "0.4.14+713c266865"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+        d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api", "fabric-item-api-v1", "1.3.2+691a79b5bc"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+        d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api", "fabric-mining-level-api-v1", "1.0.6+d7c144a8f4"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+        d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api", "fabric-tag-extensions-v0", "1.2.8+d7c144a8f4"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+        d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api", "fabric-api-base", "0.4.3+d7c144a8bc"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+        // DataBreaker
+        d.addMaven("https://maven.gegy.dev/", new MavenId("supercoder79:databreaker:0.2.8"), ModDependencyFlag.RUNTIME);
+        // Tiefix
+        d.addMaven("https://api.modrinth.com/maven/", new MavenId("maven.modrinth:tiefix:1.9.0"), ModDependencyFlag.RUNTIME);
+    }
+
+    @Override
+    public int getJavaVersion() {
+        // Default is Java 8
+        return 17;
+    }
+
+    @Override
+    public BrachyuraDecompiler decompiler() {
+        // Uses QuiltFlower instead of CFR
+        return new FernflowerDecompiler(Maven.getMavenJarDep("https://maven.quiltmc.org/repository/release", new MavenId("org.quiltmc:quiltflower:1.7.0"))); 
+    };
+
+    @Override
+    public Path getBuildJarPath() {
+        // Changes the jar file name
+        return getBuildLibsDir().resolve(getModId() + "-" + "mc" + getMcVersion() + "-" + getVersion() + ".jar");
+    }
+
+    @Override
+    public ProcessorChain resourcesProcessingChain() {
+        // Overrides the version tag in fabric.mod.json
+        return new ProcessorChain(super.resourcesProcessingChain(), new FmjVersionFixer(this));
+    }
+}
---
+++ src/main/java/uk/debb/ultra_pickaxe/UltraPickaxe.java
@@ -0,0 +1,9 @@
+package uk.debb.ultra_pickaxe;
+
+import net.fabricmc.api.ModInitializer;
+
+public class UltraPickaxe implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
